4bc87dd3bb4599bd123d48778db3eeccc7b7c1fb
avinprasad650-arch <avinprasad650@gmail.com>
2026-08-10 18:14:32 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+{
+  "name": "ghl-phone-validator",
+  "version": "1.0.0",
+  "description": "NumLookupAPI to GHL Tag Injector",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "axios": "^1.6.0"
+  }
+}
